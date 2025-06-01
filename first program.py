dbace00cf27f151d7e74e1691a6d7dbe5777688e
DEE2465 <120559098+DEE2465@users.noreply.github.com>
2025-06-01 08:05:37 +0530

    Add files via upload
    my first program , I did 5 questions of different different topics 
    1. Input and Output
    2. Data Types and Type Conversion
     3. Variable Assignment and Naming
    4. Swapping Variables
     5. Comments and Multiline Strings
     6. Basic Print Formatting

---
+++ first program.py
@@ -0,0 +1,70 @@
+"""Exam is going on today so lets start the question paper solve
+so the first question is that
+Q1= Write a program to display a person's name , age , and address in three different lines """
+"""name = input("enter your name ")
+print (name)
+
+age = int(input("Enter your age here: "))
+print(age)
+
+
+address = input("Enter your address here: ")
+print(address)
+
+name= "john"
+age = 18
+address = "kalli pahari 29"
+print (name)
+print (age)
+print ( address)"""
+
+#Q2= Write a programm to swap two variable
+"""X = 12
+Y = 13
+temp = X
+print (X)
+X = Y
+print (X)
+Y = temp
+print (Y)
+print ("value of X is",X)
+print ("value of Y is",Y)"""
+
+#METHOD 2
+"""a = 20
+b = 30
+a,b = b,a
+print (a)
+print (b)
+print (" value of a is",a)
+print ("value of b is ",b)"""
+
+
+#Q3= Write a program to convert float into integer
+"""x = 12.2
+print (type(x))
+
+x = int(x)
+print (type(x))
+print (x)"""
+
+#Q4= Write a prograam to take details from a student for id card and then print it also in different lines
+"""print ('student id card')
+name = input("enter your name here")
+print (name)
+age = int(input("enter your age here "))
+print (age)
+RollNumber = int(input("enter your RollNumber here"))
+print (RollNumber)
+address = input("enter your address here")
+print (address)"""
+
+#Q5= Write a program to take an user input as integer then convert it into float
+"""age = int(input("enter your age here"))
+print (age)
+print (type(age))
+
+age = float(age)
+print (type(age))
+print ("the type of age is",type(age))"""
+
